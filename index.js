188ab268e15ff8c8cf91c5389e4db13e9c394eb6
Harshit Bamotra <harshitbamotra.01@gmail.com>
2025-07-07 01:23:23 +0530

    Removing usless packages

--- index.js
+++ index.js
@@ -1,34 +1,6 @@
-// const express = require("express");
 const path = require("path");
 
-// const {PORT} = require("./config/server.config");
 const CppTestGenerator = require("./generators/cppTestGenerator");
-
-// const app = express();
-
-// app.listen(PORT, async ()=>{
-//     console.log("server listening on port: ", PORT);
-//     // codeReader();
-//     const testGenerator = new CppTestGenerator({root:__dirname});
-//     testGenerator.initialize();
-
-//     const cppFiles = await testGenerator.scanCppProject(path.resolve(__dirname, "../codebase"));
-
-//     const initialTests = await testGenerator.generateInitialTests(cppFiles);
-
-//     const refinedTests = await testGenerator.refineTests(initialTests);
-
-//     const buildResult = await testGenerator.buildAndDebugTests(path.resolve(__dirname, "../codebase"), refinedTests);
-
-//     const finalTests = testGenerator.finalTests;
-    
-//     const report = testGenerator.generateReport(finalTests, buildResult.coverage);
-    
-//     console.log(report);
-    
-//     // const buildResult = await testGenerator.runTestsAndCalculateCoverage();
-//     // testGenerator.buildProject();
-// })
 
 async function main(){
     const generator = new CppTestGenerator({root: __dirname});
